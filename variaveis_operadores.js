2348c861fdfdac3bddefa01d42ca5a0d1accbf7e
Leticia Betman <leticiarodriguesbetman@gmail.com>
2025-07-03 22:44:54 -0300

    feat: adiciona entradas do usuário com readline-sync

--- variaveis_operadores.js
+++ variaveis_operadores.js
@@ -1,37 +1,35 @@
-//Exercício 1
-let salario = 10000.00;
-let abono = 1000.00;
+import * as readline from 'readline-sync';
 
-let novoSalario = salario + abono;
+// Exercício 1
+const salario = Number(readline.question("Digite o salario: "));
+const abono = Number(readline.question("Digite o abono: "));
 
-console.log("\nO novo salário é: " + novoSalario.toFixed(2));
+const novoSalario = salario + abono;
+console.log("\nO novo salario e: " + novoSalario.toFixed(2));
 
-//Exercício 2
-let nota1 = 10.0;
-let nota2 = 8.0;
-let nota3 = 7.0;
-let nota4 = 7.5;
+// Exercício 2
+const nota1 = Number(readline.question("Digite a nota 1: "));
+const nota2 = Number(readline.question("Digite a nota 2: "));
+const nota3 = Number(readline.question("Digite a nota 3: "));
+const nota4 = Number(readline.question("Digite a nota 4: "));
 
-let media = (nota1 + nota2 + nota3 + nota4) / 4;
-
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
 console.log("\nMédia final: " + media.toFixed(1));
 
-//Exercício 3
-let salarioBruto = 2000.00;
-let adicionalNoturno = 500.00;
-let horasExtras = 100.00;
-let descontos = 200.00;
+// Exercício 3
+const salarioBruto = Number(readline.question("Digite o salario bruto: "));
+const adicionalNoturno = Number(readline.question("Digite o adicional noturno: "));
+const horasExtras = Number(readline.question("Digite o valor das horas extras: "));
+const descontos = Number(readline.question("Digite os descontos: "));
 
-let salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
-
+const salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
 console.log("\nSalário Líquido: " + salarioLiquido.toFixed(2));
 
-//Exercício 4
-let n1 = 5.0;
-let n2 = 6.0;
-let n3 = 7.0;
-let n4 = 8.0;
+// Exercício 4
+const n1 = Number(readline.question("Digite o número n1: "));
+const n2 = Number(readline.question("Digite o número n2: "));
+const n3 = Number(readline.question("Digite o número n3: "));
+const n4 = Number(readline.question("Digite o número n4: "));
 
-let diferenca = (n1 * n2) - (n3 * n4);
-
+const diferenca = (n1 * n2) - (n3 * n4);
 console.log("\nDiferença: " + diferenca.toFixed(1));
